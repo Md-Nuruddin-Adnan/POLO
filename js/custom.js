dda7c6aeb79ba7393cf6d47c8abde85034f455c7
Md. Nuruddin Adnan <nuruddin.adnan1993@gmail.com>
2019-11-29 18:49:19 +0600

    Full responsive with mobile header

---
+++ js/custom.js
@@ -0,0 +1,48 @@
+$(function(){
+    //sticky menu
+     var menuDis = $(".header").offset().top;
+     
+     $(window).scroll(function(){
+        var scrollValue = $(this).scrollTop();
+         
+         if(scrollValue >= menuDis){
+             $(".header").addClass("position-fixed");
+             $(".banner").addClass("bannerMargin");
+         }else{
+             $(".header").removeClass("position-fixed");
+             $(".banner").removeClass("bannerMargin");  
+         }
+     });
+ });
+
+
+ $(function(){
+    //wow jquery plugins
+    new WOW().init();
+});
+
+
+//nav small device
+$(document).ready(function() {
+    $('.header.mobile .navbar-toggler').click(function() {
+      $('.header.mobile .sidebar').toggleClass('active');
+      $('.header.mobile  span').toggleClass('active');
+      $('.header.mobile .navbar-toggler ').toggleClass('toggle');
+    })
+  });
+  
+ 
+
+
+  // Init slick slider + animation
+$('.banner-slider').slick({
+    autoplay: true,
+    autoplaySpeed: 7000,
+    speed: 2000,
+    arrows: true,
+    dots: true,
+    focusOnSelect: true,
+    pauseOnHover:false,
+    prevArrow: '<span class="prev-arrow"><i class="fas fa-angle-left"></i></span>',
+    nextArrow: '<span class="next-arrow"><i class="fas fa-angle-right"></i></span>',
+  }).slickAnimation();
